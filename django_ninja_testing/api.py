27451c3c3b8eebd1ed10a41735d335bc3e999c48
Artur Mostowski <artur.mostowski@protonmail.com>
2024-02-18 13:51:41 +0100

    response test

---
+++ django_ninja_testing/api.py
@@ -0,0 +1,30 @@
+from ninja import NinjaAPI, Schema
+
+api = NinjaAPI()
+
+class UserSchema(Schema):
+    """
+    This is the description of the schema that should be visible in swagger
+    """
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+
+class Error(Schema):
+    """
+    This is the description of the schema that should be visible in swagger ERROR
+    """
+    message: str
+
+
+@api.post("/hello", response={200: UserSchema, 403: Error}, openapi_extra = {
+        "responses": {
+            200: {"description": "You will receive this response if everything is ok"},
+            403: {"description": "You don't have what it takes for this API"}
+        }
+    })
+def hello(request, body: UserSchema):
+    if body.username == 'error':
+        return 403, Error(message='error')
+    return 200, body
