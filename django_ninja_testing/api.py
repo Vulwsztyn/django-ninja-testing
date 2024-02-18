783ee87058e47d69ff8ff9b92590660166c795cd
Artur Mostowski <artur.mostowski@protonmail.com>
2024-02-18 14:04:33 +0100

    extra keys test

--- django_ninja_testing/api.py
+++ django_ninja_testing/api.py
@@ -1,30 +1,14 @@
 from ninja import NinjaAPI, Schema
+from pydantic import BaseModel, ConfigDict
 
 api = NinjaAPI()
 
 class UserSchema(Schema):
-    """
-    This is the description of the schema that should be visible in swagger
-    """
+    model_config = ConfigDict(extra='allow')
     username: str
-    email: str
-    first_name: str
-    last_name: str
 
-class Error(Schema):
-    """
-    This is the description of the schema that should be visible in swagger ERROR
-    """
-    message: str
-
-
-@api.post("/hello", response={200: UserSchema, 403: Error}, openapi_extra = {
-        "responses": {
-            200: {"description": "You will receive this response if everything is ok"},
-            403: {"description": "You don't have what it takes for this API"}
-        }
-    })
+@api.post("/hello", response={200: UserSchema})
 def hello(request, body: UserSchema):
-    if body.username == 'error':
-        return 403, Error(message='error')
+    print(body)
+    print(body.__pydantic_extra__)
     return 200, body
