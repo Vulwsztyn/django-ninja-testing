4f6c1fe877935e8c891bce64667a594b4a3bc884
Artur Mostowski <artur.mostowski@protonmail.com>
2024-02-19 13:46:25 +0100

    check if pydantic behaves the same

--- django_ninja_testing/api.py
+++ django_ninja_testing/api.py
@@ -1,5 +1,6 @@
 from ninja import NinjaAPI, Schema
 from pydantic import BaseModel, ConfigDict
+import json
 
 api = NinjaAPI()
 
@@ -7,8 +8,20 @@ class UserSchema(Schema):
     model_config = ConfigDict(extra='allow')
     username: str
 
+class User(BaseModel):
+    model_config = ConfigDict(extra='allow')
+
+    username: str
+
+
+
 @api.post("/hello", response={200: UserSchema})
 def hello(request, body: UserSchema):
     print(body)
     print(body.__pydantic_extra__)
+    request_body = json.loads(request.body)
+    user_from_schema = UserSchema(**request_body)
+    print(user_from_schema)
+    user_pydantic = User(**request_body)
+    print(user_pydantic)
     return 200, body
